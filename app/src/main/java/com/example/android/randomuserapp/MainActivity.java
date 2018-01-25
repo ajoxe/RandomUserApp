109c6234f89432bbee5e32845e4d31aa7cf97657
Amirah Oxendine <amirahoxendine@ac.c4q.nyc>
2018-01-24 20:12:52 -0500

    added fragments to main activity

--- app/src/main/java/com/example/android/randomuserapp/MainActivity.java
+++ app/src/main/java/com/example/android/randomuserapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.android.randomuserapp;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.android.randomuserapp.fragments.UserDetailFragment;
+import com.example.android.randomuserapp.fragments.UserListFragment;
+
 public class MainActivity extends AppCompatActivity {
+    UserListFragment userListFragment;
+    UserDetailFragment userDetailFragment;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userDetailFragment = new UserDetailFragment();
+        userListFragment = new UserListFragment();
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_fragment_container, userListFragment);
+        fragmentTransaction.addToBackStack("next");
+        fragmentTransaction.commit();
     }
 }
